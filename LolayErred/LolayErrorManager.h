88a962fc92f9fb4524eb765ba8a875cdd66caf8b
Scott A Andrew <scott@mymotiv.com>
2018-01-11 16:41:37 -0800

    Remove the alert display code not being used and has been deprecated.

--- LolayErred/LolayErrorManager.h
+++ LolayErred/LolayErrorManager.h
@@ -36,19 +36,6 @@
 */
 - (id) initWithDomain:(NSString*) inDomain;
 
-- (NSString*) titleForError:(NSError*) error;
-- (NSString*) messageForError:(NSError*) error;
-- (NSString*) buttonTextForError:(NSError*) error;
-- (void) presentErrorCode:(NSInteger) code;
-- (void) presentErrorCode:(NSInteger) code suffix:(NSString*) suffix;
-- (void) presentErrorCode:(NSInteger) code error:(NSError*) error;
-- (void) presentErrorCode:(NSInteger) code suffix:(NSString*) suffix error:(NSError*) error;
-- (void) presentError:(NSError*) error;
-- (void) presentErrors:(NSArray*) errors;
-- (void) presentError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion;
-- (void) presentError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion error:(NSError*) error;
-- (void) presentError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion title:(NSString*) title;
-- (void) presentError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion title:(NSString*) title error:(NSError*) error;
 - (NSError*) createError:(NSInteger) code;
 - (NSError*) createError:(NSInteger) code suffix:(NSString*) suffix;
 - (NSError*) createError:(NSInteger) code error:(NSError*) error;
